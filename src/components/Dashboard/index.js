d97f7fd63d2abf530322cd6fe3df998e890f318a
Nagendra Singh <nagendracoolsingh@gmail.com>
2023-10-02 17:14:03 +0530

    feat: video background

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -19,17 +19,35 @@ const Dashboard = () => {
         <h1>ABLAZED VERSE</h1>
       </div>
 
-      <div className={styles.image_container}>
-        {dashboardData.map((item) => (
-          <div
-            className={styles.pics}
-            key={item?.image}
-            onClick={() => setDetails(item)}
-          >
-            <img src={item?.image} alt="image" style={{ width: "100%" }} />
-            <div className={styles.card_text}>{item?.desc}</div>
+      <div>
+        <div
+          className={styles.image_container}
+          style={{ position: "relative" }}
+        >
+          <div className={styles.video_container}>
+            <video
+              autoPlay
+              muted
+              loop
+              width="100%"
+              height="100%"
+              className={styles.video_styles}
+            >
+              <source src="/startsMoving.mp4" type="video/mp4" />
+            </video>
           </div>
-        ))}
+
+          {dashboardData.map((item) => (
+            <div
+              className={styles.pics}
+              key={item?.image}
+              onClick={() => setDetails(item)}
+            >
+              <img src={item?.image} alt="image" style={{ width: "100%" }} />
+              <div className={styles.card_text}>{item?.desc}</div>
+            </div>
+          ))}
+        </div>
       </div>
 
       <Modal
