1d7484103f264a2353d32d1e7bf9eb39b8cd7101
Nagendra Singh <nagendracoolsingh@gmail.com>
2023-10-02 13:39:17 +0530

    feat: description and details modal added

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,11 +1,18 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import dashboardData from "../../../public/dashboardData";
+import { Modal } from "antd";
 
 const COMMON_IMAGE =
   "https://img.freepik.com/premium-photo/beautiful-landscape-based-3d-rendering-illustration_771975-25.jpg";
 
 const Dashboard = () => {
+  const [details, setDetails] = useState(null);
+  const handleOk = () => {
+    setDetails(null);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
@@ -14,11 +21,30 @@ const Dashboard = () => {
 
       <div className={styles.image_container}>
         {dashboardData.map((item) => (
-          <div className={styles.pics} key={item?.image}>
+          <div
+            className={styles.pics}
+            key={item?.image}
+            onClick={() => setDetails(item)}
+          >
             <img src={item?.image} alt="image" style={{ width: "100%" }} />
+            <div className={styles.card_text}>{item?.desc}</div>
           </div>
         ))}
       </div>
+
+      <Modal
+        title="Your Memory"
+        open={details}
+        onCancel={handleOk}
+        footer={null}
+        width="50%"
+        centered
+      >
+        <div>
+          <img src={details?.image} alt="image" style={{ width: "100%" }} />
+          <div>{details?.desc}</div>
+        </div>
+      </Modal>
     </div>
   );
 };
